5350a4b82044752d5472c64c6dbe98abb3c3a194
lezoudali <lezou.dali@gmail.com>
2015-04-07 14:11:29 -0400

    navbar search

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,22 @@
 class JobsController < ApplicationController
+  def index
+
+  end
+
   def new
+    @current_user = current_user
     @job = Job.new
+    @job.skills.build
+  end
+
+  def create
+    job = Job.new(job_params)
+    if job.save
+      redirect_to "/jobs"
+    end
+  end
+  private 
+  def job_params
+    params.require(:job).permit(:title, :description, skill_ids: [] )
   end
 end
