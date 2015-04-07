6ffcbd404290bff52764ce729afe97244582f6a3
myklenero <mykle.nero@gmail.com>
2015-04-07 11:21:33 -0400

    Created new job form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,11 @@
 class JobsController < ApplicationController
   def new
+    @current_user = current_user
     @job = Job.new
+    @job.skills.build
+  end
+
+  def create
+    # binding.pry
   end
 end
