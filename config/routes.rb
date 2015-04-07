5f94c3e6983708abbe7e436b15b5dd9effbbafda
myklenero <mykle.nero@gmail.com>
2015-04-07 10:42:37 -0400

    Fixed Merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: "welcome#index"
+  get 'jobs/new'
 
+  root to: "welcome#index"
   resources :sessions
+  resources :jobs
 
   get '/auth/twitter/callback', to: "sessions#create"
   get '/logout', to: 'sessions#destroy'
