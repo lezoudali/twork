ec9d49f34780f4c4dee16bcb2d60c20542fd5854
myklenero <mykle.nero@gmail.com>
2015-04-07 14:00:16 -0400

    Finished new job form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,8 @@
 class JobsController < ApplicationController
+  def index
+
+  end
+
   def new
     @current_user = current_user
     @job = Job.new
@@ -6,6 +10,13 @@ class JobsController < ApplicationController
   end
 
   def create
-    # binding.pry
+    job = Job.new(job_params)
+    if job.save
+      redirect_to "/jobs"
+    end
+  end
+  private 
+  def job_params
+    params.require(:job).permit(:title, :description, skill_ids: [] )
   end
 end
