2155f12ecde7c9c214be4e6e281e313cca747bda
lezoudali <lezou.dali@gmail.com>
2015-04-13 03:45:56 -0400

    fixed it a bit

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,7 +3,7 @@ class Search
 
   def self.for(keyword)
     keyword.downcase!
-    users  = User.where("lower(name) LIKE ?", "%#{keyword}%")
+    users  = User.where("lower(first_name) LIKE ? OR lower(last_name) LIKE ?", "%#{keyword}%", "%#{keyword}%")
     jobs   = Job.where("lower(title) LIKE ?", "%#{keyword}%")
     skills = Skill.where("lower(name) LIKE ?", "%#{keyword}%")
     {users: users, jobs: jobs, skills: skills}
