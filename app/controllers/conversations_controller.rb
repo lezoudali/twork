f03fdd3cd039258d798441e78023d87443c92633
lezoudali <lezou.dali@gmail.com>
2015-04-13 02:35:46 -0400

    fixing small things

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,7 +6,7 @@ class ConversationsController < ApplicationController
   def create
     recipients = User.where(id: conversation_params[:recipients])
     conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
-    flash[:success] = "Your message was successfully sent!"
+    flash[:notice] = "Your message was successfully sent!"
     redirect_to conversation_path(conversation)
   end
 
