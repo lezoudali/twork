77b0b846c9b4b94192b9f73f5ab680ba72fe4d93
lezoudali <lezou.dali@gmail.com>
2015-04-12 21:01:49 -0400

    skills fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,30 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'flatiron-twork.herokuapp.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain, 
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    domain:               'heroku.com', 
+    enable_starttls_auto: true
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
